7cc524664590634e19f785b5647669da18b1d9d5
Osama_mousa <osamamousa048@gmail.com>
2022-02-15 15:32:51 -0500

    JS notes

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 const url = 'http://localhost:3000/movies' 
 console.log ('hello world')
+
+//create a function where an item is created / aka lsiten for form submit and display item
+
+
+//create a delete button / aka. listen for different actions / delete / edit / update / cancel change
+
+
+//mark as complete 
+
+
+
+//import moment library for "time stamp"
+
+
+
+//display movie list in alphabtical order
+
