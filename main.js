8eb3ee1f9a4a32e20acf2d0571463f19ae02d2a4
Osama_mousa <osamamousa048@gmail.com>
2022-02-16 14:37:52 -0500

    phase3

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const url = 'http://localhost:3000/movies' 
 console.log ('hello world')
+const form = document.getElementById("mform")
 
 //create a function where an item is created / aka lsiten for form submit and display item
-
+form.addEventListener("submit", function (event){
+    event.preventDefault()
+    const movieText = document.getElementById("mtext").value
+    createMovie(movieText)
+})
 
 //create a delete button / aka. listen for different actions / delete / edit / update / cancel change
 
